a65bc1b368c015a7b27aa28b4cfa944bc32d4694
kaiocotrim <kaioalan40@gmail.com>
2024-09-11 00:32:14 -0300

    okay

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 100 
+console.log(`a velocidade do seu carro é ${vel}km por hora `)
+if (vel > 80){
+    console.log(`voce utrapassou a velocida permitida. multado !`)
+} else { 
+    console.log(`A velocida marcada do seu veiculo foi de ${vel}`)
+}
+console.log(`dirija sempre usando sinto de segurança !`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = `BR`
+console.log(`Vocé mora no ${pais}`)
+if (pais == "BR"){
+    console.log(`Você é Brasileiro`)
+} else {
+    console.log(`Você é estrangeiro`)
+}
