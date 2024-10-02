003bf895b009b56c8b5669f6ec811a12b17b5852
kaiocotrim <kaioalan40@gmail.com>
2024-10-01 23:48:09 -0300

    up

--- aula16ex/modelo - Copia/script.js
+++ aula16ex/modelo - Copia/script.js
@@ -13,22 +13,27 @@ function isnumero(n) {
 
 
 
-function inlista(n, 1) {
-    if (l.indexOf(Number(n)) ! = -1 ) {
+function inlista(n, l) {
+    if (l.indexOf(Number(n)) != -1 ) {
         return true
     }  else {
         return false
     }
 }
 
-
+<!
 
 function adicionar(){
     if (isnumero(num.value) && !inlista (num.value, valores)){
-        Window.alert ('tudo okay')
-
+        valores.push(number(num.value))
+        let item = document.createElement('option')
+        intem.text = `Valor' ${num.value} adicionado.`
+        lista.appendChild(item)
     } else {
         window.alert ('O valor já foi encontrado na lista .')
     }
+
+    num.value = ''
+    num.focus()
 
 }
